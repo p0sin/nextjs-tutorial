882cccf7cceb815d565d3ef82094d29452cca043
p0sin <elposin@gmail.com>
2025-02-03 15:27:56 -0600

    Error Handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
@@ -10,19 +11,23 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         fetchCustomers(),
     ]);
 
-  return (
+    if (!invoice) {
+        notFound();
+    }
+
+    return (
     <main>
-      <Breadcrumbs
+        <Breadcrumbs
         breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
-          {
+            { label: 'Invoices', href: '/dashboard/invoices' },
+            {
             label: 'Edit Invoice',
             href: `/dashboard/invoices/${id}/edit`,
             active: true,
-          },
+            },
         ]}
-      />
-      <Form invoice={invoice} customers={customers} />
+        />
+        <Form invoice={invoice} customers={customers} />
     </main>
-  );
+    );
 }
